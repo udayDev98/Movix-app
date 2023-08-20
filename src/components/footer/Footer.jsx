7d3fb8c65c2a5d88570856a2f0421befb7e216f8
unknown <neerajup1998@gmail.com>
2023-08-21 00:27:14 +0530

    Movix-app

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import "./style.scss";
+import { useNavigate } from "react-router-dom";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                    Welcome to our movie site, your ultimate destination for all things cinema! 
+                    Whether you're a die-hard film enthusiast or just looking for some entertainment, 
+                    we've got you covered. Explore our diverse collection of movie reviews, trailers, and more.
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
